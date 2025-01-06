4d81e86c21f434ec26e4291cfeb9a3cd33708f71
KNXCSdev <michal.miksho@gmail.com>
2025-01-06 19:42:49 +0100

    React Toast, Creating Cabin,ReactHookForm

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,10 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import { getCabins } from "../services/apiCabins";
 import CabinTable from "../features/cabins/CabinTable";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
+import Button from "../ui/Button";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <>
       <Row type="horizontal">
@@ -14,6 +18,9 @@ function Cabins() {
 
       <Row>
         <CabinTable></CabinTable>
+
+        <Button onClick={() => setShowForm((show) => !show)}>Add new Cabin</Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
