1f1fe742706137b8ccbff0abc8567ee2723d1fd6
KNXCSdev <michal.miksho@gmail.com>
2025-01-10 22:41:30 +0100

    User Authoriaztion,Logging Out

---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,40 @@
+import { useNavigate } from "react-router";
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+import styled from "styled-components";
+import { useEffect } from "react";
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+  // 1. Load the authenticated user
+  const { isLoading, isAuthenticated } = useUser();
+
+  // 2. if there is no authenticated user,redirect to login page
+  useEffect(
+    function () {
+      if (!isAuthenticated && !isLoading) navigate("/login");
+    },
+    [isAuthenticated, isLoading, navigate]
+  );
+
+  // 3. While loading, show spinner
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+
+  // 4. If there is a user Render the app
+  if (isAuthenticated) return children;
+}
+
+export default ProtectedRoute;
